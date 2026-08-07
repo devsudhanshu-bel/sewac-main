11c9f279d25a0227b1413d0bf366361aa5011a50
devsudhanshu-bel <belavarthy.sudhanshu@gmail.com>
2026-08-07 11:12:07 +0530

    feat(complaints): initialize complaints page and replace AI layout

---
+++ src/pages/Complaints.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/layouts/Header";
+
+export default function Complaints() {
+  return (
+    <>
+      <Header />
+
+      <div className="px-8 py-8">
+        {/* Complaints page content will go here */}
+      </div>
+    </>
+  );
+}
