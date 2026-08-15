351914cb2fc31ca2836ced4282b423f63defc928
Ashutosh Dash <dasashu2004@gmail.com>
2026-08-15 17:59:34 +0530

    Updated Complaints page and endpoints

--- src/services/filterService.js
+++ src/services/filterService.js
@@ -1,14 +1,29 @@
 import api from "../api/axios";
 
-export const getCities = () => api.get("/api/master-citizen/cities");
+export const getCities = async () => {
+  const res = await api.get("/api/master-citizen/cities");
 
-export const getZones = (cityId) =>
-  api.get(`/api/master-citizen/cities/${cityId}/zones`);
+  return Array.isArray(res.data) ? res.data : res.data?.data || [];
+};
 
-export const getDivisions = (cityId, zoneId) =>
-  api.get(`/api/master-citizen/cities/${cityId}/zones/${zoneId}/divisions`);
+export const getZones = async (cityId) => {
+  const res = await api.get(`/api/master-citizen/cities/${cityId}/zones`);
 
-export const getWards = (cityId, zoneId, divisionId) =>
-  api.get(
+  return Array.isArray(res.data) ? res.data : res.data?.data || [];
+};
+
+export const getDivisions = async (cityId, zoneId) => {
+  const res = await api.get(
+    `/api/master-citizen/cities/${cityId}/zones/${zoneId}/divisions`,
+  );
+
+  return Array.isArray(res.data) ? res.data : res.data?.data || [];
+};
+
+export const getWards = async (cityId, zoneId, divisionId) => {
+  const res = await api.get(
     `/api/master-citizen/cities/${cityId}/zones/${zoneId}/divisions/${divisionId}/wards`,
   );
+
+  return Array.isArray(res.data) ? res.data : res.data?.data || [];
+};
