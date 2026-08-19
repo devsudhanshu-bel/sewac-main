d1fcf0ee9adcf53e4e947c206ab947860a7e6b2b
devsudhanshu-bel <belavarthy.sudhanshu@gmail.com>
2026-08-19 14:32:34 +0530

    Integrated Customer Grievances - 3

--- src/components/overview/CustomerGrev.jsx
+++ src/components/overview/CustomerGrev.jsx
@@ -31,8 +31,7 @@ import "leaflet/dist/leaflet.css";
 ============================================================ */
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:5002";
+  import.meta.env.VITE_API_BASE_URL 
 
 
 /*
