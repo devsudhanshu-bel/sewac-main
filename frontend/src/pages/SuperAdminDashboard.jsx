bcd6567d5edac48bcf9736e03d7347465b0b61a2
Ashutosh Dash <dasashu2004@gmail.com>
2026-08-19 16:05:31 +0530

    Updated Phone Number field

--- frontend/src/pages/SuperAdminDashboard.jsx
+++ frontend/src/pages/SuperAdminDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { X } from "lucide-react";
-import API_BASE_URL from "./services/api";
+import API_BASE_URL from "../services/api";
 
 export default function CreateAdminModal({
   onClose,
